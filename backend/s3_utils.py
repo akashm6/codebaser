cf97155f8d5420961b213d5ae4e7dbd0684c61d8
Akash Mohan <akhikm4@gmail.com>
2025-07-29 15:14:17 -0500

    generates presigned urls for bucket upload

--- backend/s3_utils.py
+++ backend/s3_utils.py
@@ -1,24 +1,42 @@
+from botocore.exceptions import ClientError
+from botocore.config import Config
+from dotenv import load_dotenv
+from datetime import datetime
 import boto3
 import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
 BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
 SECRET_ACCESS_KEY = os.getenv("S3_SECRET_ACCESS_KEY")
+REGION_NAME = os.getenv("S3_REGION_NAME")
+
+s3_config = Config(
+    signature_version='v4'
+)
 
 s3 = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_ACCESS_KEY
+    aws_secret_access_key=SECRET_ACCESS_KEY,
+    region_name = REGION_NAME,
+    config=s3_config,
 )
 
-file_name = 's3test.txt'
-key = "test-folder/s3test.txt"
+def generate_presigned_url(filename: str, content_type: str):
 
-s3.upload_file(file_name, BUCKET_NAME, key)
-print(f"Uploaded {file_name} to s3://{BUCKET_NAME}/{key}")
-
-s3.download_file(BUCKET_NAME, key, "downloaded.txt")
-print("Downloaded back to downloaded.txt")
+    timestamp = datetime.utcnow().time()
+    key_url = f"{timestamp}_{filename}"
+    
+    try: 
+        presigned = s3.generate_presigned_url(
+        "put_object",
+        Params = {'Bucket': BUCKET_NAME, "Key": key_url, "ContentType": content_type},
+        ExpiresIn=300
+    )
+    except ClientError as e:
+        print(e)
+        return
+    
+    return presigned
