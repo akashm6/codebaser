11df5b16b61a2bbd1318b2756041c4c3a757e710
Akash Mohan <akhikm4@gmail.com>
2025-07-29 15:13:14 -0500

    endpoints for s3 presigned urls

---
+++ backend/routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Query
+from dotenv import load_dotenv
+from s3_utils import generate_presigned_url
+import os
+
+
+router = APIRouter()
+
+
+@router.get("/generate-presigned-url")
+def get_presigned_url(filename: str = Query(...), content_type: str = Query(...)):
+    return generate_presigned_url(filename, content_type)
+
+@router.get("/")
+def server_test():
+    return "Server running correctly."
+
+
