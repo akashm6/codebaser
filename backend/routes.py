e616edf41071c3bd29b076861b511a9851770e7b
Akash Mohan <akhikm4@gmail.com>
2025-07-30 19:53:53 -0500

    concurrent zip file parsing works

--- backend/routes.py
+++ backend/routes.py
@@ -1,15 +1,23 @@
+from parallel_processing.multiprocessor import concurrent_parse
+from s3_utils import generate_presigned_url, s3
 from fastapi import APIRouter, Query
 from dotenv import load_dotenv
-from s3_utils import generate_presigned_url
 from pydantic import BaseModel
+import tempfile
+import zipfile
+import boto3
 import os
 
+load_dotenv()
+
 class ZipFileModel(BaseModel):
+    s3_key: str
     name: str
     content_type: str
     size: int
 
 router = APIRouter()
+BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 @router.get("/generate-presigned-url")
 def get_presigned_url(filename: str = Query(...), content_type: str = Query(...)):
@@ -18,11 +26,24 @@ def get_presigned_url(filename: str = Query(...), content_type: str = Query(...)
 @router.post("/zip-processing")
 def process_zip(zip: ZipFileModel):
 
-    name = zip.name
-    content_type = zip.content_type
-    size = zip.size
-    return {"name": name, "content_type": content_type, "size": size, "message": "hello!!!"}
+    s3_key = zip.s3_key
+    # frontend has uploaded zip to s3, fetch the s3 file and download a temp file from it
+    with tempfile.NamedTemporaryFile(suffix='.zip', delete=False) as temp_file:
 
+        temp_path = temp_file.name
+        s3.download_file(BUCKET_NAME, s3_key, temp_path)
+        # unzip and begin ast parsing
+        with zipfile.ZipFile(temp_path, "r") as z:
+            base_path = temp_path.replace(".zip", "")
+            z.extractall(base_path)
+            chunks = concurrent_parse(base_path)
+            return {
+                "name": zip.name,
+                "content_type": zip.content_type,
+                "size": zip.size,
+                "num_chunks": len(chunks),
+                "message": "Successfully parsed zip"
+            }
 
 @router.get("/")
 def server_test():
