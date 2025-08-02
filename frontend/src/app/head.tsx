233560a18c81bee6ddc95c8cee910a6d172a8d22
Akash Mohan <akhikm4@gmail.com>
2025-08-01 23:50:58 -0500

    ograph metadata

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -2,11 +2,12 @@ export default function Head() {
   return (
     <>
       <title>codebaser</title>
-      <meta name="description" content="Navigate unfamiliar codebases!" />
       <meta property="og:title" content="codebaser – Understand Any Codebase" />
       <meta property="og:description" content="Navigate unfamiliar codebases by asking questions on what you're confused about!" />
-      <meta property="og:url" content="https://codebaser.vercel.app/" />
-      <meta property="og:type" content="website" />
+      <meta property="og:image" content="<generated>" />
+<meta property="og:image:type" content="<generated>" />
+<meta property="og:image:width" content="<generated>" />
+<meta property="og:image:height" content="<generated>" />
     </>
   );
 }
