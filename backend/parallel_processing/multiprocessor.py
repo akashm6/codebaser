ee8e1e5e40832b44ec9427578aa1463a88731314
Akash Mohan <akhikm4@gmail.com>
2025-08-01 19:53:18 -0500

    one-time cache of lang objects for faster parsing

--- backend/parallel_processing/multiprocessor.py
+++ backend/parallel_processing/multiprocessor.py
@@ -1,6 +1,7 @@
 from parallel_processing.codechunk import chunk_go, chunk_java, chunk_javascript, chunk_python, chunk_typescript
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from tree_sitter import Parser, Language
+import multiprocessing
 from pathlib import Path
 from typing import List
 import os
@@ -23,46 +24,44 @@ CHUNK_MAP = {
     "go": chunk_go
 }
 
+LANGUAGE_OBJECTS = {
+    lang_name: Language(LIB_PATH, lang_name)
+    for lang_name in CHUNK_MAP.keys()
+}
+
 def parse_and_chunk_file(file_path: str) -> List[dict]:
     extension = os.path.splitext(file_path)[1]
-    file_lang = LANG_MAP.get(extension, None)
+    file_lang = LANG_MAP.get(extension)
     if not file_lang:
         return []
-    
-    chunk_function = CHUNK_MAP.get(file_lang, None)
+
+    chunk_function = CHUNK_MAP.get(file_lang)
     if not chunk_function:
         return []
 
     parser = Parser()
-    language = Language(LIB_PATH, file_lang)
-    parser.set_language(language)
+    parser.set_language(LANGUAGE_OBJECTS[file_lang]) 
+
     code = Path(file_path).read_text()
     tree = parser.parse(bytes(code, 'utf-8'))
 
-    chunks = chunk_function(tree, code, file_path)
-    return chunks
+    return chunk_function(tree, code, file_path)
 
-def concurrent_parse(source_dir: str, max_workers: int = 8):
+def concurrent_parse(source_dir: str, max_workers: int = None):
+    if not max_workers:
+        max_workers = multiprocessing.cpu_count()
+        
     file_paths = []
     for root, _, files in os.walk(source_dir):
         for f in files:
             extension = os.path.splitext(f)[1]
             if extension in LANG_MAP:
                 file_paths.append(os.path.join(root, f))
-
-    #print(f"discovered {len(file_paths)} code files")
+                
     chunks = []
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(parse_and_chunk_file, file_path) for file_path in file_paths]
         for future in as_completed(futures):
             chunks.extend(future.result())
-    # logging
-    '''
-    count = 1
-    for c in chunks:
-        print(f"CHUNK #{count}")
-        print("=" * 40)
-        print(c)
-        count += 1
-    '''
+    
     return chunks
